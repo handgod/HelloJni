a709f0198c40d9437bf19bb73483901813b75a0d
handgod <943488158@qq.com>
2016-03-01 09:43:42 +0800

    init
    compile success,in eclipse mars,with ndk-r10e

---
+++ obj/local/armeabi/objs-debug/hello-jni/hello-jni.o.d
@@ -0,0 +1,170 @@
+/home/andy/Documents/eclipseworkspace/HelloJni/obj/local/armeabi/objs-debug/hello-jni/hello-jni.o: \
+ /home/andy/Documents/eclipseworkspace/HelloJni/jni/hello-jni.c \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/jni.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/stdio.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/sys/cdefs.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/sys/cdefs_elf.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/android/api-level.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/sys/_types.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/machine/_types.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/sys/types.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/stdint.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/linux/posix_types.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/linux/stddef.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/linux/compiler.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/asm/posix_types.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/asm/types.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/linux/types.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/machine/kernel.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/sys/sysmacros.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/stdlib.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/string.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/malloc.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/alloca.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/strings.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/memory.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/asm/ptrace.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/asm/user.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/asm/page.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/sys/wait.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/sys/resource.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/linux/resource.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/linux/time.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/asm/resource.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/asm-generic/resource.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/linux/wait.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/sys/mman.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/asm/mman.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/asm-generic/mman.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/asm-generic/mman-common.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/dlfcn.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/dirent.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/unistd.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/sys/select.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/sys/time.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/signal.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/limits.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/sys/limits.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/linux/limits.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/machine/internal_types.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/machine/limits.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/sys/syslimits.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/asm/signal.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/asm-generic/signal.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/asm/siginfo.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/asm-generic/siginfo.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/sys/sysconf.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/linux/capability.h \
+ /usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/pathconf.h
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/jni.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/stdio.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/sys/cdefs.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/android/api-level.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/sys/_types.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/machine/_types.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/sys/types.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/stdint.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/linux/posix_types.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/linux/stddef.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/linux/compiler.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/asm/posix_types.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/asm/types.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/linux/types.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/machine/kernel.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/sys/sysmacros.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/stdlib.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/string.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/malloc.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/alloca.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/strings.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/memory.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/asm/ptrace.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/asm/user.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/asm/page.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/sys/wait.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/sys/resource.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/linux/resource.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/linux/time.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/asm/resource.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/asm-generic/resource.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/linux/wait.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/sys/mman.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/asm/mman.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/asm-generic/mman.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/asm-generic/mman-common.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/dlfcn.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/dirent.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/unistd.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/sys/select.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/sys/time.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/signal.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/limits.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/sys/limits.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/linux/limits.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/machine/internal_types.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/machine/limits.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/sys/syslimits.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/asm/signal.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/asm-generic/signal.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/asm/siginfo.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/asm-generic/siginfo.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/sys/sysconf.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/linux/capability.h:
+
+/usr/ndk/android-ndk-r8d/platforms/android-5/arch-arm/usr/include/pathconf.h:
